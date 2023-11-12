183556599251796c2b3caada33a5314d69162006
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-11-12 16:06:17 -0500

    Update

--- src/graphql/root.graphql.ts
+++ src/graphql/root.graphql.ts
@@ -13,11 +13,11 @@ const goodReadsApiService = new GoodreadsApiService()
 export const root: { Query: Schema } = {
   Query: {
     githubStatus: () =>
-      cacheManager.cachedReponse('githubStatus', () => githubAPI.getGithubStatus(), 60 * 10 /* 10 minutes */),
+      cacheManager.cachedReponse('githubStatus', () => githubAPI.getGithubStatus(), 2),
     latestCommit: () =>
-      cacheManager.cachedReponse('latestCommit', () => githubAPI.getLatestCommit(), 60 * 5 /* 5 minutes */),
-    listening: () => cacheManager.cachedReponse('listening', () => lastFMAPI.getSongListening(), 30 /* 30 seconds */),
+      cacheManager.cachedReponse('latestCommit', () => githubAPI.getLatestCommit(), 2),
+    listening: () => cacheManager.cachedReponse('listening', () => lastFMAPI.getSongListening(), 60 * 60),
     reading: () =>
-      cacheManager.cachedReponse('reading', () => goodReadsApiService.lastReading(), 60 * 30 /* 30 minutes */),
+      cacheManager.cachedReponse('reading', () => goodReadsApiService.lastReading(), 2
   },
 }
